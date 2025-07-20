0a397f55376aec228ae2a34bf25b235e4dc136ad
Targib Sajid <167701006+TargibSajid@users.noreply.github.com>
2025-07-20 20:29:02 +0600

    Change of folder
    Change of Folders

---
+++ Mid/form.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Lab Task</title>
+</head>
+
+<body>
+    <center>
+        <h1 style="color: blue;">AIUB</h1>
+        <h2 style="color: blue;">Registration Form</h2>
+    </center>
+
+    <h3>Complete the Registration</h3>
+
+ 
+        <table>
+            <tr>
+                <td>Full Name:</td>
+
+                <td> </td>
+                <td><input type="text" name="fullname"></td>
+            </tr>
+
+            <tr>
+                <td>Email:</td>
+
+                <td> </td>
+                <td><input type="email" name="email"></td>
+            </tr>
+
+            <tr>
+                <td>Password:</td>
+                <td> </td>
+                <td><input type="password" name="password"></td>
+            </tr>
+
+            <tr>
+                <td>Gender:</td>
+
+                <td> </td>
+                <td>
+                    <input type="radio" > Male
+                    <input type="radio" > Female
+                    <input type="radio" > Other
+                </td>
+            </tr>
+
+            <tr>
+                <td>Languages Known:</td>
+                <td> </td>
+                <td>
+                    <input type="checkbox" > English
+                    <input type="checkbox" > Bangla
+                    <input type="checkbox" > Hindi
+                </td>
+            </tr>
+
+            <tr>
+                <td>Country:</td>
+                <td> </td>
+                <td>
+                    <select name="country">
+                        <option value="">-Select-</option>
+                        <option value="Bangladesh">Bangladesh</option>
+                        <option value="India">India</option>
+                        <option value="China">China</option>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Date of Birth:</td>
+                <td> </td>
+                <td><input type="date" ></td>
+            </tr>
+
+            <tr>
+                <td>Upload Your File:</td>
+                <td> </td>
+                <td><input type="file" ></td>
+            </tr>
+
+            <tr>
+                <td>Comment:</td>
+                <td> </td>
+                <td>
+                    <textarea rows="5" cols="30" ></textarea>
+                </td>
+            </tr>
+
+            <tr>
+                <td> </td>
+                <td> </td>
+                <td >
+                    <input type="submit" value="Register">
+                    
+                </td>
+            </tr>
+        </table>
+    
+
+</body>
+</html>
+
+
+
+
+</table>
+
+</body>
+
+</html>
